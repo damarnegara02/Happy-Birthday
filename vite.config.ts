8fb50edfeef18fb0a96fb223e919d0302e9a8b91
Damar Negara <damarnegara2002@gmail.com>
2025-09-05 18:14:55 +0800

    Update package.json & Vite config for Railway deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
@@ -11,21 +10,21 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": "/client/src",
+      "@shared": "/shared",
+      "@assets": "/attached_assets",
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: "client", // folder tempat index.html React
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "../dist/frontend", // relatif ke root project
     emptyOutDir: true,
   },
   server: {
